ef9c562e5ca7eb59fff35d30991ef4cbcd042f3e
dopecello <williamlocke.cello@gmail.com>
2022-06-26 13:37:17 -0400

    defined some variables at the top

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,16 @@
+var characterLength = 8;
+var choiceArray =[];
+
+var specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '|', '~'];
+var lowerArr =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]; 
+var upperArr =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+var numberArr =['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  console.log("You clicked the button!")
-
-  return "Generated password will go here.";
-}
+//Add Event Listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -18,6 +20,18 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  console.log("You clicked the button!")
+  // 1. Prompt user for the password criteria
+    //    a. Password length 1-128
+ let passwordLength = window.prompt("Type in password length (8-128)")
+ console.log(passwordLength)
+  //    b. then, lower, upper, numbers, special char.
+  // 2. Validate the input,
+  // 3. Generate Password
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+  // 4. Display password on the page.
+
+  return "Generated password will go here.";
+}
